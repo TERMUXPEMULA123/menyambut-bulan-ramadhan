1ba5bd692d744c3cb62c80a04fc25a63cc8f49b1
MR.PS1KOP4T <79355260+TERMUXPEMULA123@users.noreply.github.com>
2021-03-25 20:06:20 +0700

    Add files via upload

---
+++ yambutbulanramadan.sh
@@ -0,0 +1,234 @@
+#!/bin/sh
+#gk usah di record bikin sendiri gampang
+
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+clear
+echo
+toilet -f big -F gay RHAMADAN
+sleep 1
+echo $purple"╔══════════════════════╗"
+echo $purple"║╔════════════════════╗╚╗"
+echo $purple"║║██░░░░░░░░░░░░░░░░░░╚╗╚╗"
+echo $putple"║║██RAMADHAN BERKAH░░░░ ─║║║"
+echo $purple"║║██░░░░░░░░░░░░░░░░░░╔╝╔╝"
+echo $purple"║╚════════════════════╝╔╝"
+echo $purple"╚══════════════════════╝"
+sleep 1
+echo $green"  RAMADHAN TIBA HATI GEMBURA"
+sleep 1
+echo $blue"{=============================}"
+echo
+sleep 1
+echo $res"APA YANG KALIAN TUNGGU DARI RAMADAN"
+echo $blue"֍֍֍֍֍֍֍֍֍֍֍֍֍֍֍֍֍֍֍֍֍֍֍֍֍֍֍֍֍֍֍֍֍֍֍֍"
+echo
+echo $white"1.) PERSIAPAN BULAN RAMADAN"
+echo $white"2.) puasa enaknya ngapain"
+echo $white"3.) ngabuburit enaknya kemana"
+echo $white"4.) bukber enaknya sama siapa"
+echo $white"5.) cara menahan lapar dan haus"
+echo $white"6.) enaknya kalo sahur pake apa"
+echo
+sleep 1
+read -p "masukan nomor:" gan
+
+if [ $gan = 1 ]
+then
+clear
+sleep 1
+echo
+echo
+echo $purple"╔══════════════════════╗"
+echo $purple"║╔════════════════════╗╚╗"
+echo $purple"║║██░░░░░░░░░░░░░░░░░░╚╗╚╗"
+echo $putple"║║██RAMADHAN BERKAH░░░░ ─║║║"
+echo $purple"║║██░░░░░░░░░░░░░░░░░░╔╝╔╝"
+echo $purple"║╚════════════════════╝╔╝"
+echo $purple"╚══════════════════════╝"
+echo
+echo
+sleep 2
+toilet -f mini -F gay ASSALAMUALAIKUM
+sleep 2
+toilet -f mini -F gay MARHABAN
+sleep 1
+toilet -f mini -F gay YA SYAHRUH
+sleep 2
+toilet -f mini -F gay RAMADHAN
+sleep 2
+toilet -f mini -F gay YAHSYA
+sleep 1
+toilet -f mini -F gay RO SHIYAM
+sleep 2
+echo
+echo $blue"᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥"
+echo $green" *AUTHOR   :   REDI SIMARMATA*"
+echo $green" *CNL YT   :   KAGAK ADA"
+echo $green" *SCRIPT   :   SPECIAL RAMADHAN"
+echo $blue"᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥"
+echo
+fi
+if [ $gan = 2 ]
+then
+clear
+sleep 1
+echo $blue"᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥"
+echo $green" *AUTHOR   :   REDI SIMARMATA*"
+echo $green" *CNL YT   :   KAGAK ADA"
+echo $green" *SCRIPT   :   SPECIAL RAMADHAN"
+echo $blue"᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥"
+echo
+sleep 5
+echo $blue"HEHH ELU MANUSIA APA BUKAN NGAB........................"
+sleep 5
+echo
+echo $green"SORY CUY GW TYPO WKWKWK..............................."
+sleep 5
+echo
+echo $purple"1.pagi tidur sampe siang tapi jangan kelamaan....."
+sleep 5
+echo
+echo $cyan"2.jangan lupa juga untuk shalat dan ngaji supaya berkah.."
+sleep 5
+echo
+echo $res"3.kalo sore ngabuburit dong ama temen atau keluarga"
+sleep 5
+echo
+echo $white"4.malemnya perang sarung jika mampu wkwk......"
+sleep 5
+echo
+echo $yellow"5.nah lebih berkah lagi kalo kita bangunin orang sahur..."
+sleep 5
+echo
+echo $blue"(makasih sudah pakai sc saya semoga anda calon penghuni syurga:)"
+echo
+sleep 1
+fi
+if [ $gan = 3 ]
+then
+clear
+sleep 1
+echo
+echo
+echo $blue"᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥"
+echo $green" *AUTHOR   :   REDI SIMARMATA*"
+echo $green" *CNL YT   :   KAGAK ADA"
+echo $green" *SCRIPT   :   SPECIAL RAMADHAN"
+echo $blue"᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥"
+echo
+sleep 5
+echo $blue"kalo mau ngabuburit enaknya itu jam 16.00-17.30"
+sleep 5
+echo
+echo $green"ingat jangan ngabuburit ama doi nanti malah maksiat luh"
+sleep 5
+echo
+echo $purple"lebih enak lagi kalo ngabuburitnya jalan kaki broo..."
+sleep 5
+echo
+echo $cyan"kalo mau ngabuburit gw saranin jangan yg pemandangan gunung dua"
+sleep 5
+echo
+echo $white"kayaknya enak ke perairan dah soalnya adem gitu liatnya"
+sleep 5
+echo
+echo $yellow"pulangnya jangan lupa beli es campur atau apa gitu buat buka puasa"
+sleep 5
+echo
+echo $blue"(makasih karna sudah pake sc saya semoga kelak engkau masuk surga"
+sleep 1
+echo
+fi
+if [ $gan = 4 ]
+then
+clear
+sleep 1
+echo
+echo
+echo $blue"᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥"
+echo $green" *AUTHOR   :   REDI SIMARMATA*"
+echo $green" *CNL YT   :   KAGAK ADA"
+echo $green" *SCRIPT   :   SPECIAL RAMADHAN"
+echo $blue"᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥"
+echo
+sleep 1
+echo $blue"kalo elu mau bukber ajak2 gw lah supaya berkah wanjay"
+sleep 2
+toilet -f mini -F niat puasa
+sleep 2
+toilet -f mini -F insyaallah
+sleep 1
+toilet -f mini -F berkah
+sleep 1
+echo $green"wkwk gk nyambubg tapi yg penting halal"
+sleep 1
+echo
+fi
+if [ $gan = 5 ]
+then
+clear
+sleep 1
+echo
+echo $blue"᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥"
+echo $green" *AUTHOR   :   REDI SIMARMATA*"
+echo $green" *CNL YT   :   KAGAK ADA"
+echo $green" *SCRIPT   :   SPECIAL RAMADHAN"
+echo $blue"᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥"
+echo
+sleep 1
+echo $yellow"Jangan jadikan puasa sebagai halangan untuk melakukan aktivitas harian"
+sleep 6
+echo
+echo $cyan"Menonton Film Seru Favorit Anda....."
+sleep 5
+echo
+echo $red"Tidak Melihat Jam Berkali-kali. ..."
+sleep 4
+echo
+echo $purple"Perbanyak Makan Buah dan Sayur. ..."
+sleep 4
+echo
+echo $green"Tidak Terlalu Makan Asin dan Manis Saat Berbuka. ..."
+sleep 5
+echo
+echo $white"e...h manis mah gpp kan sunah wkwk...."
+sleep 5
+echo
+echo $blue"SEMOGA PUASAMU LANCAR YAH NGAB..."
+sleep 1
+echo
+fi
+if [ $gan = 6 ]
+then
+clear
+sleep 1
+echo
+echo $blue"᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥"
+echo $green" *AUTHOR   :   REDI SIMARMATA*"
+echo $green" *CNL YT   :   KAGAK ADA"
+echo $green" *SCRIPT   :   SPECIAL RAMADHAN"
+echo $blue"᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥᪥"
+echo
+sleep 1
+echo $yellow"AYAM GORENG TEPUNG : hayo siapa yg suka itu"
+sleep 4                                                                                              echo
+echo $cyan"TELUR GULUNG : fiks cocok buat elu yg gk suka ribet"                                      sleep 4
+echo
+echo $red"MIE INSTAN : hayo pasti banyak nih yg sahur pake mie wkwk"
+sleep 5
+echo
+echo $purple"TAHU DAN TEMPE : gpp yang penting niat puasa :)"
+sleep 5
+echo
+echo $white"DAH ITU YG SERING DI MAKAN BUAT LAUK SAHUR..."
+echo
+sleep 1
+fi
